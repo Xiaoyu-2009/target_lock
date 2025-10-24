4f02b5e5ca0cc8a33b082d53fe7758cdbfc7ffe4
Xiaoyu_2009 <3551371733@qq.com>
2025-10-24 22:46:58 +0800

    正式提交

--- src/main/java/net/xiaoyu/target_lock/client/ClientSetupHandler.java
+++ src/main/java/net/xiaoyu/target_lock/client/ClientSetupHandler.java
@@ -1,9 +1,9 @@
 package net.xiaoyu.target_lock.client;
 
+import net.xiaoyu.target_lock.TargetLock;
 import net.neoforged.bus.api.SubscribeEvent;
 import net.neoforged.fml.common.EventBusSubscriber;
 import net.neoforged.fml.event.lifecycle.FMLClientSetupEvent;
-import net.xiaoyu.target_lock.TargetLock;
 
 @EventBusSubscriber(modid = TargetLock.MOD_ID)
 public class ClientSetupHandler {
--- src/main/java/net/xiaoyu/target_lock/client/TargetLockHandler.java
+++ src/main/java/net/xiaoyu/target_lock/client/TargetLockHandler.java
@@ -6,6 +6,7 @@ import net.xiaoyu.target_lock.Config;
 import net.xiaoyu.target_lock.util.EntityUtils;
 import net.xiaoyu.target_lock.util.MessageUtils;
 import net.xiaoyu.target_lock.util.RotationUtils;
+import net.xiaoyu.target_lock.util.LockTargetData;
 import net.minecraft.client.Minecraft;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.player.Player;
@@ -17,8 +18,7 @@ import net.neoforged.neoforge.common.NeoForge;
 
 @EventBusSubscriber(modid = TargetLock.MOD_ID)
 public class TargetLockHandler {
-    private static Entity lockedTarget = null;
-    private static boolean isTargetLocked = false;
+    private static final LockTargetData lockData = new LockTargetData();
 
     public static void init() {
         NeoForge.EVENT_BUS.register(TargetLockHandler.class);
@@ -29,20 +29,19 @@ public class TargetLockHandler {
         Minecraft mc = Minecraft.getInstance();
 
         if (ModKeyBindings.TARGET_LOCK_KEY.consumeClick() && mc.player != null) {
-            if (isTargetLocked) {
-                isTargetLocked = false;
-                lockedTarget = null;
+            if (lockData.isTargetLocked()) {
+                lockData.reset();
                 MessageUtils.showUnlockMessage(mc);
             } else {
                 lockNearestEntity(mc);
             }
         }
 
-        if (isTargetLocked && Config.USE_NEAREST_ENTITY_PRIORITY.get() && mc.player != null) {
+        if (lockData.isTargetLocked() && Config.USE_NEAREST_ENTITY_PRIORITY.get() && mc.player != null) {
             findAndLockNearestEntity(mc);
         }
 
-        if (isTargetLocked && lockedTarget != null && !lockedTarget.isAlive() && 
+        if (lockData.isTargetLocked() && lockData.getLockedTarget() != null && !lockData.getLockedTarget().isAlive() && 
             Config.SWITCH_TO_NEXT_TARGET_AFTER_KILL.get() && mc.player != null
         ) {
             switchToNextNearestTarget(mc);
@@ -51,7 +50,7 @@ public class TargetLockHandler {
     
     @SubscribeEvent
     public static void onMovementInputUpdate(MovementInputUpdateEvent event) {
-        if (!isTargetLocked || lockedTarget == null) {
+        if (!lockData.isTargetLocked() || lockData.getLockedTarget() == null) {
             return;
         }
         
@@ -61,8 +60,15 @@ public class TargetLockHandler {
         double lockRange = Config.LOCK_RANGE.get();
         double lockRangeSquared = lockRange * lockRange;
 
-        if (lockedTarget.isAlive() && lockedTarget.distanceToSqr(player) <= lockRangeSquared) {
-            RotationUtils.updatePlayerRotationToTarget(player, lockedTarget.getEyePosition());
+        if (lockData.isLockedTargetValid() && lockData.getLockedTarget().distanceToSqr(player) <= lockRangeSquared) {
+            if (Config.LOCK_ENTITY_POSITION.get()) {
+                if (lockData.getLockedTargetPosition() == null) {
+                    lockData.setLockedTargetPosition(lockData.getLockedTarget().getEyePosition());
+                }
+                RotationUtils.updatePlayerRotationToTarget(player, lockData.getLockedTargetPosition());
+            } else {
+                RotationUtils.updatePlayerRotationToTarget(player, lockData.getLockedTarget().getEyePosition());
+            }
             return;
         }
 
@@ -70,21 +76,19 @@ public class TargetLockHandler {
     }
 
     private static void handleInvalidTarget(Player player, Minecraft mc) {
-        if (!lockedTarget.isAlive()) {
+        if (!lockData.getLockedTarget().isAlive()) {
             MessageUtils.showTargetKilledMessage(mc);
 
             if (Config.SWITCH_TO_NEXT_TARGET_AFTER_KILL.get()) {
                 switchToNextNearestTarget(mc);
                 return;
             } else {
-                isTargetLocked = false;
-                lockedTarget = null;
+                lockData.reset();
                 return;
             }
         }
 
-        isTargetLocked = false;
-        lockedTarget = null;
+        lockData.reset();
         MessageUtils.showTargetLostMessage(mc);
     }
 
@@ -93,8 +97,9 @@ public class TargetLockHandler {
         Entity nearestEntity = EntityUtils.findNearestLivingEntity(mc, lockRange);
         
         if (nearestEntity != null) {
-            lockedTarget = nearestEntity;
-            isTargetLocked = true;
+            lockData.setLockedTarget(nearestEntity);
+            lockData.setTargetLocked(true);
+            lockData.setLockedTargetPosition(null);
             MessageUtils.showLockSuccessMessage(mc, nearestEntity);
         } else {
             MessageUtils.showNoTargetsMessage(mc);
@@ -105,23 +110,24 @@ public class TargetLockHandler {
         double lockRange = Config.LOCK_RANGE.get();
         Entity nearestEntity = EntityUtils.findNearestLivingEntity(mc, lockRange);
         
-        if (nearestEntity != null && nearestEntity != lockedTarget) {
-            lockedTarget = nearestEntity;
+        if (nearestEntity != null && nearestEntity != lockData.getLockedTarget()) {
+            lockData.setLockedTarget(nearestEntity);
+            lockData.setLockedTargetPosition(null);
             MessageUtils.showSwitchTargetMessage(mc, nearestEntity);
         }
     }
 
     private static void switchToNextNearestTarget(Minecraft mc) {
         double lockRange = Config.LOCK_RANGE.get();
-        Entity nearestEntity = EntityUtils.findNearestLivingEntity(mc, lockRange, lockedTarget);
+        Entity nearestEntity = EntityUtils.findNearestLivingEntity(mc, lockRange, lockData.getLockedTarget());
         
         if (nearestEntity != null) {
-            lockedTarget = nearestEntity;
-            isTargetLocked = true;
+            lockData.setLockedTarget(nearestEntity);
+            lockData.setTargetLocked(true);
+            lockData.setLockedTargetPosition(null);
             MessageUtils.showSwitchToNextTargetMessage(mc, nearestEntity);
         } else {
-            isTargetLocked = false;
-            lockedTarget = null;
+            lockData.reset();
             MessageUtils.showNoNextTargetMessage(mc);
         }
     }
